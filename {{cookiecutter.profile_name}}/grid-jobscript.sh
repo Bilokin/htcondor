340033587c97066298a08e09439d4f86c431f74d
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2021-05-03 19:11:18 +0200

    Invert if

--- {{cookiecutter.profile_name}}/grid-jobscript.sh
+++ {{cookiecutter.profile_name}}/grid-jobscript.sh
@@ -7,9 +7,10 @@ echo "hostname:"
 hostname -f
 echo $BELLE2_RELEASE_DIR
 
-if [ -z "$BELLE2_RELEASE" ]; then
+if [[ -z "$BELLE2_RELEASE" ]]; then
+  echo "No basf2 release defined!"
+else
   source /cvmfs/belle.cern.ch/tools/b2setup.sh $BELLE2_RELEASE
 fi
-
 
 {exec_job}
